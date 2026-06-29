2eda3a5213d6d2d389eb6e4e5e73ca2ec16a7f0b
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-06-29 21:03:09 +0530

    e:\DSA(150-QUES)\Ques_DSA\59_Group_Anagrams_HashMap.cpp

---
+++ 59_Group_Anagrams_HashMap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+
+    unordered_map<string, vector<string>> mp;
+
+    for(string s : strs) {
+        string temp = s;
+        sort(temp.begin(), temp.end());
+        mp[temp].push_back(s);
+    }
+
+    for(auto it : mp) {
+        for(string word : it.second)
+            cout << word << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
